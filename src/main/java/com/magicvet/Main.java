c8d90c3088d863e48682a0f8f709189c6adac2ff
AngelinaGanzha <angelina_chkan@ukr.net>
2024-04-29 20:58:58 +0300

    REQ-02: Email validation

--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -1,23 +1,37 @@
 package main.java.com.magicvet;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     static String password = "default";
 
     static Scanner scanner = new Scanner(System.in);
+
+    static String email_pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
     public static void main(String[] args) {
         run();
     }
 
     static void run (){
+        if (auth()) {
+            registerNewClient();
+        }
+
+    }
+
+
+
+    static boolean auth() {
         boolean accepted = false;
-        for (int i = 0; i <3; i ++){
+        for (int i = 0; i < 3; i++) {
             System.out.print("Password: ");
             String input = scanner.nextLine();
 
-            if (password.equals(input)){
+            if (password.equals(input)) {
                 accepted = true;
                 break;
             } else {
@@ -25,5 +39,37 @@ public class Main {
             }
         }
         System.out.println(accepted ? "Welcome to the Magic  Vet!" : "Application has been blocked.");
+        return accepted;
+    }
+    static void registerNewClient() {
+        System.out.println("Please, provide client details.");
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        if (isEmailValid(email)) {
+            Client client = buildClient(email);
+            System.out.println("New client: " + client.firstName + " " + client.lastName + " (" + client.email + ")");
+
+        } else {
+            System.out.println("Provided email is invalid");
+        }
+    }
+
+     static Client buildClient(String email) {
+         Client client = new Client();
+         client.email = email;
+
+         System.out.print("First name: ");
+         client.firstName = scanner.nextLine();
+
+         System.out.print("Last name: ");
+         client.lastName = scanner.nextLine();
+         return client;
+     }
+
+     static boolean isEmailValid(String email) {
+        Pattern pattern = Pattern.compile(email_pattern);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
