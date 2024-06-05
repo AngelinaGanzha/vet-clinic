c32d330d32f6982be5b09b20e940cf7ae33c5b69
AngelinaGanzha <angelina_chkan@ukr.net>
2024-06-05 19:15:19 +0300

    REQ-10: Add a few clients and print them (grouped by cities)

--- src/main/java/com/magicvet/model/Client.java
+++ src/main/java/com/magicvet/model/Client.java
@@ -9,14 +9,16 @@ public class Client {
     private String lastName;
     private String email;
     private List<Pet> pets = new ArrayList<>();
+    private Location location;
 
     @Override
     public String toString() {
         return "Client {"
-                + "\n\tfirstName + " + firstName
-                + ", lastName + " + lastName
-                + ", email + " + email
-                + ",\n\tpets + " + pets
+                + "\n\tfirstName = " + firstName
+                + ", lastName = " + lastName
+                + ", email = " + email
+                + ", location = " + location
+                + ",\n\tpets = " + pets
                 + "\n}";
     }
 
@@ -60,15 +62,27 @@ public class Client {
         return email;
     }
 
-    public List<Pet> getPet() {
+    public List<Pet> getPets() {
         return pets;
     }
 
-    public void setPet(List<Pet> pets) {
+    public void setPets(List<Pet> pets) {
         this.pets = pets;
     }
 
     public void addPet(Pet pet){
         pets.add(pet);
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public enum Location {
+        Kyiv, Lviv, Odesa
     }
 }
